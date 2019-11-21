419558a74b4c2b944423aa5b5a1bc7358baf049c
shyboylpf <lpf.mart@gmail.com>
2019-11-21 16:23:36 +0800

    简化写法

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Hello world."/> -->
+    <test1 isMorning=false />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import test1 from './components/test1.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+    test1
   }
+  
 }
 </script>
 
---
+++ src/components/test1.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p v-if=isMorning> 早上好 </p>
+        <p v-if=isAfternoon> 早上好 </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'test1',
+  props: {
+    isMorning: Boolean,
+    isAfternoon:Boolean
+  }
+}
+</script>
